3f25c885ead2107b25c64d577324dd9f4b0f3da5
kratnapagol <86149572+kratnapagol@users.noreply.github.com>
2022-11-01 00:14:08 +0530

    Update Water-Leakage-Detector.ino

--- Water-Leakage-Detector/Water-Leakage-Detector.ino
+++ Water-Leakage-Detector/Water-Leakage-Detector.ino
@@ -138,3 +138,23 @@ void pulseCounter1()
   // Increment the pulse counter
   pulseCount1++;
 }
+
+#
+
+void setup() {
+  Serial.begin(9600);
+  SPI.begin();
+  SPI.setDataMode(SPI_MODE0); //CPHA = CPOL = 0    MODE = 0
+  delay(1000);
+
+  pinMode(CS_PIN, OUTPUT);
+  pinMode(ADXL_INT1_PIN, INPUT);
+
+  adxl372_Get_DevID(&adxl372, &devId);
+
+  Serial.print("Device id: ");
+  Serial.println(devId, HEX);
+
+  adxl372_Set_Op_mode(&adxl372, FULL_BW_MEASUREMENT);
+  Set_Impact_Detection();
+}
